b0e1fdc6b79f412f6eedd2e66928344eb90250e2
Kutaeva Yuliya <kutaeva.yuliya@gmail.com>
2023-11-21 00:51:03 +0300

    Новый коммит

--- Yuliya_Kutaeva_maven_at_project/src/test/java/homework/day13/TestDayOfWeekUtilityJunit.java
+++ Yuliya_Kutaeva_maven_at_project/src/test/java/homework/day13/TestDayOfWeekUtilityJunit.java
@@ -1,5 +1,12 @@
 package homework.day13;
 
+import homework.DayOfWeekUtility;
+import org.junit.Assert;
+
 public class TestDayOfWeekUtilityJunit {
 
+        public void checkGetDayOfWeekNextYear(){
+
+           //Assert.assertThat(,,DayOfWeekUtility.getDayOfWeekNextYear(1));
+    }
 }
