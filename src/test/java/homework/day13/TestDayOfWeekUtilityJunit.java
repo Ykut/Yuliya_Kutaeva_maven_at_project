6670148fdf820f8816e21e71380d6ed1fa388fc3
Kutaeva Yuliya <kutaeva.yuliya@gmail.com>
2023-11-25 10:08:32 +0300

    Новый коммит

---
+++ src/test/java/homework/day13/TestDayOfWeekUtilityJunit.java
@@ -0,0 +1,16 @@
+package homework.day13;
+
+import homework.DayOfWeekUtility;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class TestDayOfWeekUtilityJunit {
+    //int
+
+
+
+
+
+}
